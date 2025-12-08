207db3de1c7447594bc9edc5f6bac6196b8f761a
laecio22 <laecon22@hotmail.com>
2025-12-08 19:49:07 -0300

    style:finalização  estilização  do  header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,10 +1,19 @@
-
+import s from "./header.module.scss";
+import { Link } from "react-router-dom";
 const Header = () => {
   return (
-    <header>
-           esse  é o  header
+    <header className={s.header}>
+      <div>
+        <img src='/coracao.png' alt="imagem coração" />
+         <Link to="/"><p>Médicos & Dentistas</p></Link>
+      
+      </div>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/voluntarios"><button>Seja Voluntário</button></Link>
+      </nav>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
