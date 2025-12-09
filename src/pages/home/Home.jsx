9bd22183b92951b8c2a03d58d856ce5ee5e3857e
laecio22 <laecon22@hotmail.com>
2025-12-09 12:40:50 -0300

    style:estilização  da sessão  principal  da Home

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,29 @@
+import s from "./home.module.scss";
+import iconSaude from "../../assets/saude.png";
+import doutor from "../../assets/doutor.png";
 
 const Home = () => {
   return (
     <main>
-        <h1>Home</h1>
+      <section className={s.container}>
+        <div className={s.boxInfo}>
+          <img src={iconSaude} alt="imagem com uma mensagem saúde para todos" />
+          <h1>Saúde e cuidado sem barreiras</h1>
+          <p>
+            Um projeto dedicado a oferecer atendimento médico e odontológico
+            gratuito para pessoas que mais precisam na nossa comunidade.
+          </p>
+          <div className={s.boxButtons}>
+            <button className={s.btnVoluntario}>Seja Voluntário</button>
+            <button className={s.btnAjuda}> Como ajudar</button>
+          </div>
+        </div>
+        <div className={s.boxImage}>
+          <img src={doutor} alt="Imagem de  um doutor  sorrindo" />
+        </div>
+      </section>
     </main>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
