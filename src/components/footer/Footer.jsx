dc170884e87aeec9c2eff67eac265861bba4e34a
laecio22 <laecon22@hotmail.com>
2025-12-11 21:34:22 -0300

    style:finalização  responsividade  do componente footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -8,28 +8,30 @@ const Footer = () => {
   return (
     <footer className={s.footer}>
       <div className={s.contentFooter}>
-        <article>
-          <div className={s.boxFlexivel}>
-            <img src={IconCoracao} alt="icone de coração" />
-            <h3>Médicos e Dentistas</h3>
-          </div>
-          <p>Saúde e cuidado sem barreiras para toda comunidade.</p>
-        </article>
-        <article>
-          <h3>Contato</h3>
-          <div className={s.boxFlexivel}>
-            <img src={IconEmail} alt="ícone  de  email" />
-            <p>contato@medico-dentista.org</p>
-          </div>
-          <div className={s.boxFlexivel}>
-            <img src={IconPhone} alt="ícone  de telefone" />
-            <p>(11) 3000-0000</p>
-          </div>
-          <div className={s.boxFlexivel}>
-            <img src={IconMap} alt="ícone de localização" />
-            <p>São Paulo, Brasil</p>
-          </div>
-        </article>
+        <div className={s.container}>
+          <article>
+            <div className={s.boxFlexivel}>
+              <img src={IconCoracao} alt="icone de coração"  className={s.imageCoracao}/>
+              <h3>Médicos e Dentistas</h3>
+            </div>
+            <p>Saúde e cuidado sem barreiras para toda comunidade.</p>
+          </article>
+          <article>
+            <h3>Contato</h3>
+            <div className={s.boxFlexivel}>
+              <img src={IconEmail} alt="ícone  de  email" />
+              <p>contato@medico-dentista.org</p>
+            </div>
+            <div className={s.boxFlexivel}>
+              <img src={IconPhone} alt="ícone  de telefone" />
+              <p>(11) 3000-0000</p>
+            </div>
+            <div className={s.boxFlexivel}>
+              <img src={IconMap} alt="ícone de localização" />
+              <p>São Paulo, Brasil</p>
+            </div>
+          </article>
+        </div>
         <article>
           <h3>Redes Sociais</h3>
           <p>Facebook</p>
