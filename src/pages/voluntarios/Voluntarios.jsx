4ef15ad940eccab618d254d63bc2fc12712b3724
laecio22 <laecon22@hotmail.com>
2025-12-13 11:58:53 -0300

    style:inicio  estilização  do  formulário

--- src/pages/voluntarios/Voluntarios.jsx
+++ src/pages/voluntarios/Voluntarios.jsx
@@ -7,33 +7,36 @@ const Voluntarios = () => {
         <h1>Seja Voluntário</h1>
         <p>Junte-se a nós e faça a diferença na vida de pessoas que precisam</p>
         <div className={s.containerCards}>
-          <CardVantagens 
-          title="Impacto Direto" 
-          description="Sua dedicação salva vidas e transforma comunidades"/>
           <CardVantagens
-          title="Crescimento Pessoal" 
-          description="Desenvolva habilidades e cresça profissionalmente"
+            title="Impacto Direto"
+            description="Sua dedicação salva vidas e transforma comunidades"
           />
           <CardVantagens
-          title="Comunidade"
-          description="Faça parte de uma rede de profissionais comprometidos"
-          />        
+            title="Crescimento Pessoal"
+            description="Desenvolva habilidades e cresça profissionalmente"
+          />
+          <CardVantagens
+            title="Comunidade"
+            description="Faça parte de uma rede de profissionais comprometidos"
+          />
         </div>
       </section>
       <section className={s.containerForm}>
         <form>
           <h2>Inscrição para Voluntários</h2>
-          <div>
+          <div className={s.groupInput}>
             <h4>Dados Pessoais</h4>
-            <input type="text"  placeholder="Nome*" />
-            <input type="email" placeholder="Seu Email*" />
-            <input type="tel" placeholder="Seu telefone" />
+            <div className={s.boxFlexivel}>
+              <input type="text" placeholder="Nome*" />
+              <input type="email" placeholder="Seu Email*" />
+            </div>
+            <input type="tel" placeholder="Seu telefone*" />
           </div>
-          <div>
+          <div className={s.groupInput}>
             <h4>Mensagem Adicional</h4>
             <textarea placeholder="Conte-nos porque quer ser  um voluntário ..."></textarea>
           </div>
-          <p>Entraremos em  contato para  mais informações</p>
+          <p>Entraremos em contato para mais informações</p>
           <button>Enviar Inscrição</button>
         </form>
       </section>
